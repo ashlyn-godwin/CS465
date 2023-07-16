f6234bbb8e204b214d2abc9bd36b0c0b4d4edf9e
Ashlyn Godwin <samuel.godwin@snhu.edu>
2023-07-15 21:40:42 -0500

    Changes to MVC and early implementation of JSON based travel descriptions

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,9 @@
+const { isUtf8 } = require("buffer");
+const fs = require("fs");
+const trips = JSON.parse(fs.readFileSync("app_server/data/trips.json", "utf8")); // parse JSON, read to filesystem
+
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways - Travel'});
+    res.render('travel', {title: 'Travlr Getaways - Travel', trips});
 };
 module.exports = {
     travel
