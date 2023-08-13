dfd7765195a8b751863d35f7d20fbb8bd8aac963
Ashlyn Godwin <samuel.godwin@snhu.edu>
2023-08-13 02:21:33 -0500

    addTrips() implemented

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 // import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
   styleUrls: ['./trip-listing.component.css'],
   providers: [TripDataService]
 })
+
 export class TripListingComponent implements OnInit {
   // trips: Array<any> = trips;
   trips: Trip[];
   message: string;
-  constructor(private tripDataService: TripDataService) { }
+
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+    ) { }
+
   private getTrips(): void {
     console.log('Inside TripListingComponent#getTrips');
     this.message = 'Searching for trips';
@@ -24,6 +32,12 @@ export class TripListingComponent implements OnInit {
         this.trips = foundTrips;
       });
   }
+
+  private addTrip():void {
+    console.log('Inside TripListingComponent#AddTrip');
+    this.router.navigate(['add-trip']);
+  }
+
   ngOnInit(): void {
     this.getTrips();
   }
